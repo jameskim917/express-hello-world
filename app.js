a8caf65de8d601c06ca283ad8ba16c2c6498a207
James Kim <jameskim917@gmail.com>
2023-11-01 19:59:47 +0900

    init

--- app.js
+++ app.js
@@ -1,61 +1,94 @@
+// import { Request, Response } from "express";
+// import { createClient } from '@supabase/supabase-js';
+
+const { createClient } = require('@supabase/supabase-js');
+
 const express = require("express");
+const bodyParser = require('body-parser');
+
+
 const app = express();
+// Use JSON body parser middleware
+app.use(bodyParser.json());
+
+
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient('https://ljbnqgogykskettvuhrb.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxqYm5xZ29neWtza2V0dHZ1aHJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg4MjgyMzksImV4cCI6MjAxNDQwNDIzOX0.gcWU3Nqhb6gxbrWXt5Cde_D2CRbcy5DTjrmZLasfFBY');
+
+// const { data, error } = await supabase.auth.getSession();
+// https://supabase.com/docs/reference/javascript/installing
+
+// GET /api/
+
+// npm install ts-node --save-dev
+// npx ts-node app.ts
+
+app.get('/api/v1/journal', async (req, res) => {
+  // Let's GET all journal entries from the journal table of DB
+
+  const { data, error } = await supabase
+    .from('journal')
+    .select();
+
+  if (data) {
+    res.json(data);
+  }
+
+  if (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// // POST /api/
+// app.POST('/api/v1/journal', (req: Request, res: Response) => {
+//   res.json({});
+// });
+
+// // PUT /api/
+// app.PUT('/api/v1/journal', (req: Request, res: Response) => {
+//   res.json({});
+// });
+
+// // DELETE /api
+// app.DELETE('/api/v1/journal:id', (req: Request, res: Response) => {
+//   const { id } = req.params;
+//   res.json({});
+// });
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+/*
+  Objectives:
+  Map Google Data with Dayly Photos
+
+  Journal Entry - Model
+  id
+  title
+  entry
+  photos = [abc123, ...]
+  date_created
+  last_edited
+  allday
+
+  GET
+  /api/[version #]/journal
+
+  POST
+  /api/[version #]/
+
+  PUT
+  /api/[version #]/journal
+
+  DELETE
+  /api/[version #]/journal
+
+
+
+*/
